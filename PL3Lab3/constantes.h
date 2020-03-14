bce5e6a62ea305dfffe1f27da6793487204624b0
kbsplit <kbaras@uma.pt>
2020-03-14 17:27:37 +0000

    removed all occurences of constant NUM_ELEMENTOS; dynamic allocation for v; removed chars vector and related files

--- PL3Lab3/constantes.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-const int NUM_ELEMENTOS = 50;
-const int NUM_CARATERES = 80;
--- PL3Lab3/main.cpp
+++ PL3Lab3/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "opVetoresInt.h"
-#include "constantes.h"
 #include "opVetoresFloat.h"
 #include "opVetoresChar.h"
 
@@ -8,37 +7,30 @@ using namespace std;
 
 int main() {
 	int tipoDados = -1;
-	cout << "Inteiros (1), Reais (2) ou Carateres (3)?" << endl;
+	cout << "Inteiros (1) ou Reais (2)?" << endl;
 	cin >> tipoDados;
 	int tamanho;
 	if (tipoDados == 1) {
-		int v[NUM_ELEMENTOS];
-		cout << "Quantos elementos quer inserir no vector? (max. " << NUM_ELEMENTOS << ") \n";
+		
+		cout << "Quantos elementos quer inserir no vector? \n";
 		cin >> tamanho;
-		if (tamanho > 0 && tamanho <= NUM_ELEMENTOS) 
+		int * v = new int[tamanho];
+		if (v!=NULL)
 			opcoes(v, tamanho);
 		else
-			cout << "Inseriu numero de elementos invalido. Prima ENTER para terminar." << endl;
+			cout << "Nao foi possivel alocar memoria. Prima ENTER para terminar." << endl;
+		delete[] v;
 	}
 	else if (tipoDados == 2) {
-		float v[NUM_ELEMENTOS];
-		cout << "Quantos elementos quer inserir no vector? (max. " << NUM_ELEMENTOS << ") \n";
+		
+		cout << "Quantos elementos quer inserir no vector? \n";
 		cin >> tamanho;
-		if (tamanho > 0 && tamanho <= NUM_ELEMENTOS) 
+		float * v = new float[tamanho];
+		if (v!=NULL) 
 			opcoes(v, tamanho);
 		else
-			cout << "Inseriu numero de elementos invalido. Prima ENTER para terminar." << endl;
-	}
-	else if (tipoDados == 3) {
-		char frase[NUM_CARATERES];
-		unsigned numChars = leString(NUM_CARATERES, frase);
-		cout << "O numero de carateres inseridos: " << numChars << endl;
-		cout << frase;
-		//escreverDados(frase, numChars);
-		cout << endl;
-		cout << "Qual e o carater que quer eliminar da frase? " << endl;
-		char c = cin.get();
-		cout << "Frase inserida sem carater " << c << endl << strdelc(frase, c) << endl;
+			cout << "Nao foi possivel alocar memoria. Prima ENTER para terminar." << endl;
+		delete[] v;
 	}
 	else
 		cout << "Inseriu tipo de dados invalido. Prima ENTER para terminar." << endl;
